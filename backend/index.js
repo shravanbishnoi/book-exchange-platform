8c827aa7916f050b76597120cad3f8e82f0aaf13
narayan-jat <narayanjat2964@gmail.com>
2024-12-07 15:05:23 +0530

    ready for deployment

--- backend/index.js
+++ backend/index.js
@@ -1,32 +1,34 @@
-require ('dotenv').config ();
-const express = require ('express');
-const mongoose = require ('mongoose');
-const bodyParser = require ('body-parser');
-const cors = require ('cors');
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
 // Import routes
-const userRoutes = require ('./routes/userRoutes');
-const bookRoutes = require ('./routes/bookRoutes');
-const transactionRoutes = require ('./routes/transactionRoutes');
+const userRoutes = require("./routes/userRoutes");
+const bookRoutes = require("./routes/bookRoutes");
+const transactionRoutes = require("./routes/transactionRoutes");
 
-const app = express ();
-app.use (cors ());
-app.use (bodyParser.json ());
-app.use (cors ({origin: 'http://localhost:5173'}));
-
+const app = express();
+app.use(bodyParser.json());
+const allowedOrigins = process.env.IS_PRODUCTION
+  ? [process.env.PRODUCTION_FRONTEND_ORIGIN]
+  : [process.env.DEVELOPMENT_FRONTEND_ORIGIN];
+console.log(allowedOrigins);
+app.use(cors({allowedOrigins}));
 // Connect to MongoDB Atlas
 mongoose
-  .connect (process.env.MONGO_URI, {
+  .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-  .then (() => console.log ('Connected to MongoDB Atlas'))
-  .catch (err => console.error ('Failed to connect to MongoDB Atlas', err));
+  .then(() => console.log("Connected to MongoDB Atlas"))
+  .catch((err) => console.error("Failed to connect to MongoDB Atlas", err));
 
 // Routes
-app.use ('/api/users', userRoutes);
-app.use ('/api/books', bookRoutes);
-app.use ('/api/transactions', transactionRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/books", bookRoutes);
+app.use("/api/transactions", transactionRoutes);
 
 const PORT = process.env.PORT || 5000;
-app.listen (PORT, () => console.log (`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
--- front-end/src/Constants.js
+++ front-end/src/Constants.js
@@ -1,3 +1,4 @@
-export const BASE_SERVER_URL = 'http://localhost:5000/';
+export const IS_PRODUCTION = false
+export const BASE_SERVER_URL = IS_PRODUCTION ? 'https://book-exchange-platform-s50q.onrender.com/' : "http://localhost:5000/";
 export const AUTH_ENDPOINT = 'auth/';
 export const API = 'api/';
